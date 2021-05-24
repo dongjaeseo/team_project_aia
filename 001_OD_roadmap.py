6ee172b43675ef59bf6c649759f34eeab5f36088
DongJae Seo <75518803+dongjaeseo@users.noreply.github.com>
2021-05-24 18:13:41 +0900

    roadmap

---
+++ 001_OD_roadmap.py
@@ -0,0 +1,11 @@
+'''
+Andrew Ng 교수님 강의 참고
+YOLO v1 을 이해하는 로드맵
+1. Localization with classification
+2. Object Detection
+3. Sliding window(region based or conv stride)
+4. IoU
+5. NMS
+6. Anchor Box
+7. YOLO v1
+'''
